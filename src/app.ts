fcbde1c9a9bcb527073af583829886658a0645f0
oscarmaciasc <oscarmaciasprof@gmail.com>
2024-03-23 13:34:52 -0600

    Added Boom error handler as a middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose, { type ConnectOptions } from 'mongoose'
+import { logErrors, errorHandler, boomErrorHandler } from './middlewares/error.handler'
 
 const MONGO_URI = 'mongodb://localhost:27017/collaboratehub'
 
@@ -7,11 +8,16 @@ const app = express()
 const port = 3011
 
 const connectDB = () => {
-    mongoose.connect(MONGO_URI)
+  mongoose.connect(MONGO_URI)
 }
 
+app.use(express.json())
+app.use(logErrors)
+app.use(errorHandler)
+app.use(boomErrorHandler)
+
 app.get('/', (req, res) => {
-  res.send('Hello Worlddd')
+  res.send('Hello World')
 })
 
 app.listen(port, () => {
---
+++ src/middlewares/error.handler.ts
@@ -0,0 +1,25 @@
+// Boom handles HTTP errors
+
+const logErrors = (error, _req, _res, next) => {
+    console.log(error)
+
+    next(error)
+}
+
+const errorHandler = (error, _req, res, next) => {
+    res.status(500).json({
+        message: error.message,
+        stack: error.stack
+    })
+}
+
+const boomErrorHandler = (error, _req, res, next) => {
+    if(error.isBoom) {
+        const { output } = error
+        res.status(output.statusCode).json(output.payload)
+    }
+
+    next(error)
+}
+
+export { logErrors, errorHandler, boomErrorHandler }
