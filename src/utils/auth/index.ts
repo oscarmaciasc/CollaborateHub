2804c08821ebd75b3caf5b90d796cb25935df945
oscarmaciasc <oscarmaciasprof@gmail.com>
2024-03-25 22:25:09 -0600

    pushing all front and back

--- src/utils/auth/index.ts
+++
@@ -1,6 +0,0 @@
-import passport from 'passport'
-import LocalStrategy from './strategies/local.strategy'
-import JwtStrategy from './strategies/jwt.strategy'
-
-passport.use(LocalStrategy)
-passport.use(JwtStrategy)
